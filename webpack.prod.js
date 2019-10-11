1ed0d9f563f7fff1e65b553abf2380c72b51abca
zhaoshali <zhaoshali@111.com.cn>
2019-10-11 14:40:31 +0800

    feat:新增错误的回调处理

--- webpack.prod.js
+++ webpack.prod.js
@@ -157,7 +157,16 @@ module.exports = {
             ]
         }),
         new VueLoaderPlugin(),
-        new FriendlyErrorsWebpackPlugin()
+        new FriendlyErrorsWebpackPlugin(),
+        function() {
+            this.hooks.done.tap('done', (stats) => {
+                if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') == -1)
+                {
+                    console.log('build error');
+                    process.exit(1);
+                }
+            })
+        }    
         //new webpack.HotModuleReplacementPlugin()
     ].concat(htmlWebpackPlugins),
     stats: 'errors-only',
